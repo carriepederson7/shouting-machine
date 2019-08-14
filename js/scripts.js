49df55f6f981038a8de7eb9ae8ac18eebd4ed0f6
Carrie Schmid and Mia Caton <carrie@>
2019-08-14 11:02:12 -0700

    add toUpperCase

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event){
     event.preventDefault();
-  var favoriteColor = $("input#favorite-color").val();
-  var favoriteFood = $("input#favorite-food").val();
+  var favoriteColor = $("input#favorite-color").val().toUpperCase();
+  var favoriteFood = $("input#favorite-food").val().toUpperCase();
   console.log(favoriteColor);
 
   $(".favorite-color").text(favoriteColor);
+  $(".favorite-food").text(favoriteFood);
 
 
 $("#shouting").show();
